161bdeedfd0c1dd1fc4f8b3872c74b4b13849eae
Jules Rodrigues <rodrigj3@westminster.ac.uk>
2024-09-11 17:15:21 +0100

    Added placeholder logic

--- seek.py
+++ seek.py
@@ -10,7 +10,8 @@ instance = vlc.Instance()
 player = instance.media_player_new()
 
 # Define the video path and time range (in milliseconds)
-videoPath = 'video.mp4'
+videoPath = 'video.mp4' #Consider renaming
+
 video1StartTime = 1000
 video1EndTime = 3000
 
@@ -18,10 +19,17 @@ video2StartTime = 5000
 video2EndTime = 7000
 
 
+video3StartTime = 0
+video3EndTime = 0
+
+video4StartTime = 0
+video4EndTime = 0
+
+
 # Function to loop a video segment between startTime and endTime
 def LoopVideoSegment(video_path):
 
-    global video1StartTime, video2StartTime, video1EndTime, video2EndTime
+    global video1StartTime, video1EndTime, video2StartTime, video2EndTime, video3StartTime, video3EndTime, video4StartTime, video4EndTime
 
     startTime = video1StartTime
     endTime = video1EndTime
@@ -44,23 +52,27 @@ def LoopVideoSegment(video_path):
 
         # Continuously check the current playback time
         while player.get_time() < endTime:
-            current_time = player.get_time()
-            #print(f"Current time: {current_time / 1000} seconds")  # Show time in seconds
             time.sleep(0.05)  # Small delay to prevent excessive CPU usage
 
-            if(keyboard.is_pressed('esc')):
+            if (keyboard.is_pressed('esc')):
                 player.stop()
                 sys.exit(0)
 
-            elif(keyboard.is_pressed('1')):
+            elif (keyboard.is_pressed('1')):
                 startTime = video1StartTime
                 endTime = video1EndTime
                 player.set_time(startTime)
-                
-            elif(keyboard.is_pressed('2')):
+
+            elif (keyboard.is_pressed('2')):
                 startTime = video2StartTime
                 endTime = video2EndTime
                 player.set_time(startTime)
+            
+            elif (keyboard.is_pressed('3')):
+                pass # Add logic for key-press
+
+            elif (keyboard.is_pressed('4')):
+                pass # Add logic for key-press
 
         # Once the endTime is reached, the loop resets the video to the startTime
         #print(f"Looping back to {startTime / 1000} seconds")
